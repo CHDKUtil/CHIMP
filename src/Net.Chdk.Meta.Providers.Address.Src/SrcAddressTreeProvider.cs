6a4084089b507257b94b0c31c12cdcb1e22b87ad
Dmitry Shechtman <damnian@gmail.com>
2019-10-11 16:49:41 +0300

    Add all cameras with addresses to addresses.json

--- src/Net.Chdk.Meta.Providers.Address.Src/SrcAddressTreeProvider.cs
+++ src/Net.Chdk.Meta.Providers.Address.Src/SrcAddressTreeProvider.cs
@@ -15,8 +15,18 @@ namespace Net.Chdk.Meta.Providers.Address.Src
         public IDictionary<string, AddressPlatformData> GetAddresses(string path)
         {
             return GetTree(path)
-                .Where(kvp => kvp.Value!.ClearOverlay)
+                .Where(HasAddresses)
                 .ToDictionary(kvp => kvp.Key, kvp => kvp.Value!);
+        }
+
+        private static bool HasAddresses(KeyValuePair<string, AddressPlatformData?> kvp)
+        {
+            return kvp.Value!.Revisions.Any(HasAddresses);
+        }
+
+        private static bool HasAddresses(KeyValuePair<string, AddressRevisionData> kvp)
+        {
+            return kvp.Value.PaletteBufferPtr != null;
         }
     }
 }
