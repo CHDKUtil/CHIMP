efc951e889242fcc6d2e6ff3b3ce3e51ac76e437
Dmitry Shechtman <damnian@gmail.com>
2019-10-11 11:03:47 +0300

    Clean revision display format

--- src/CHIMP/CHIMP/Properties/Resources.Designer.cs
+++ src/CHIMP/CHIMP/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Chimp.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -291,15 +291,6 @@ namespace Chimp.Properties {
         public static string Camera_FirmwareVersion_Content {
             get {
                 return ResourceManager.GetString("Camera_FirmwareVersion_Content", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to {0}.{1}{2} rev {3}.{4}{5}.
-        /// </summary>
-        public static string Camera_FirmwareVersion_Format {
-            get {
-                return ResourceManager.GetString("Camera_FirmwareVersion_Format", resourceCulture);
             }
         }
         
